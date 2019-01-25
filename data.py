d0fe7ed5e6a441c2a0e66926484e26d4e1064f7e
Dahv Reinhart <dahvreinhart@hotmail.com>
2019-01-24 20:42:37 -0800

    first commit - barebones skeleton flask app with initial template and db file along with bootstrap assets

---
+++ data.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, url_for, session, redirect
+from flask_pymongo import PyMongo
+import os
+
+# Create Flask app
+template_dir = os.path.abspath('templates')
+app = Flask(__name__, template_folder=template_dir)
+
+# Connect to Mongo and initialize database variables
+app.config['MONGO_DBNAME'] = 'freshworks_duck_feeding_app'
+app.config['MONGO_URI'] = 'mongodb://admin:Mlab1134206`@ds145359.mlab.com:45359/freshworks_duck_feeding_app'
+mongo = PyMongo(app)
+
+@app.route('/')
+def render_form():
+    return render_template('user_input_form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
